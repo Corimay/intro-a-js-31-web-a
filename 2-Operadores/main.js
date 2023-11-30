4aee4dae134d78427be08710f2ef36c28cd715f8
Maythe León <leoncori16@gmail.com>
2023-11-30 14:37:37 -0600

    Incorporación de ejercicios y operadores

---
+++ 2-Operadores/main.js
@@ -0,0 +1,15 @@
+let numero = 5;
+
+console.log(numero);
+
+numero = 10;
+
+console.log(numero);
+
+console.log(10*2);
+
+alert("Esta página tiene virus");
+
+let nombreUsuario = prompt("¿Cómo te llamas?");
+
+document.write("Me llamo" + nombreUsuario);
